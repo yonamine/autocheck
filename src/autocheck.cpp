387d7e08b872268c309b2d6c3bcb3d2c36ca17a0
Djordje Todorovic <djordje.todorovic@syrmia.com>
2022-12-12 13:36:15 +0100

    autocheck: Add main driver

---
+++ src/autocheck.cpp
@@ -0,0 +1,17 @@
+//===-------- autocheck.cpp - Main driver for the tool --------------------===//
+//
+// Part of the Autocheck Project, under the Apache License v2.0 with Syrmia
+// Exceptions. See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides the implementation of the main driver.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/raw_ostream.h"
+
+int main() {
+  llvm::outs() << "=== Autocheck - Modern and Free Autosar checker\n";
+  return 0;
+}
