14371138de658458b664d72d76ffe2475ca4ebb5
Vladimir Vuksanovic <Vladimir.Vuksanovic@Syrmia.com>
2023-01-18 13:34:36 +0100

    autocheck: Update build script
    Build all LLVM targets instead of just clang. Tests have more
    dependencies like FileCheck, 'count' and 'not'. It is easier to build
    everything than to list all dependencies, and the compilation time is
    not affected by much.

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ mkdir -p build && cd build
 echo "Building Clang"
 mkdir -p llvm && cd llvm
 cmake -G "Ninja" $project_root/src/llvm-project/llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="clang"
-ninja clang
+ninja
 cd ..
 
 # compile autocheck
