5d081e096b5ac0f2d4cb4cd774c74e2bc7bf436f
Vladimir Vuksanovic <Vladimir.Vuksanovic@Syrmia.com>
2023-05-10 11:06:15 +0200

    [M2-13-2] Test character and string literals
    [M2-13-2] Octal constants (other than zero) and octal escape sequences
    (other than “\0” ) shall not be used.

--- test/Lexer/warn-octal-constant-used.cpp
+++ test/Lexer/warn-octal-constant-used.cpp
@@ -12,8 +12,23 @@ void test() {
 
   a = '\0';
   a = '\100'; // expected-warning 1 {{Octal constants (other than zero) and octal escape sequences (other than "\0") shall not be used}}
+
+  const char * const b {"\100"}; // expected-warning 1 {{Octal constants (other than zero) and octal escape sequences (other than "\0") shall not be used}}
+  
+  const char16_t c1 {u'\100'}; // expected-warning 1 {{Octal constants (other than zero) and octal escape sequences (other than "\0") shall not be used}}
+  const char32_t c2 {U'\100'}; // expected-warning 1 {{Octal constants (other than zero) and octal escape sequences (other than "\0") shall not be used}}
+  const wchar_t c3 {L'\100'}; // expected-warning 1 {{Octal constants (other than zero) and octal escape sequences (other than "\0") shall not be used}}
 }
 }
 
 // autosar-warning@6 1 {{Fixed width integer types from <cstdint>, indicating the size and signedness, shall be used in place of the basic numerical types}}
+// autosar-warning@16 1 {{Fixed width integer types from <cstdint>, indicating the size and signedness, shall be used in place of the basic numerical types}}
+// autosar-warning@18 1 {{Fixed width integer types from <cstdint>, indicating the size and signedness, shall be used in place of the basic numerical types}}
+// autosar-warning@19 1 {{Fixed width integer types from <cstdint>, indicating the size and signedness, shall be used in place of the basic numerical types}}
+// autosar-warning@16 1 {{C-style strings shall not be used}}
 // autosar-warning@5 1 {{Unused function 'test'}}
+// autosar-warning@16 1 {{Unused variable 'b'}}
+// autosar-warning@18 1 {{Unused variable 'c1'}}
+// autosar-warning@19 1 {{Unused variable 'c2'}}
+// autosar-warning@20 1 {{Unused variable 'c3'}}
+// autosar-warning@20 1 {{Type wchar_t shall not be used}}
